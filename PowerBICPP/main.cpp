8966c73432fc536548a44fba447b424bbb0ca200
Andrey Z <andrey.zalozhenkov@pikta.ru>
2022-05-14 16:39:10 +0400

    Minor optimize

--- PowerBICPP/main.cpp
+++ PowerBICPP/main.cpp
@@ -59,8 +59,8 @@ string GetINI(char *txt)
     return tmp;
 }
 
-int e_exit(const char *txt) {
-    printf("%s\n", txt);
+int e_exit(const char *txt, const char *txt1, const char *txt2) {
+    printf("%s%s%s\n", txt, txt1, txt2);
     system("pause");
     return 0;
 }
@@ -69,6 +69,8 @@ int main() {
 
     char current_work_dir[FILENAME_MAX];
 
+    TCHAR MainSettings[13] = "MainSettings";
+    TCHAR ClickerSettings[16] = "ClickerSettings";
     TCHAR browserProcess[32];
     TCHAR WheelRoll[8];
     TCHAR sleep[8];
@@ -90,55 +92,67 @@ int main() {
     //string ini = "C:\\exe\\PowerBI.ini";
 
     if (GetFileAttributes(ini.c_str()) == INVALID_FILE_ATTRIBUTES) {
-        return e_exit("PowerBI.ini not found!");
-    }
-
-    if (GetPrivateProfileString(LPCTSTR("MainSettings"), LPCTSTR("browserExeProcess"), LPCTSTR(""), browserProcess, 32, ini.c_str()) == 0) 
-        return e_exit("INI attribute browserExeProcess in MainSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("MainSettings"), LPCTSTR("mouseWheelRoll"), LPCTSTR(""), WheelRoll, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute mouseWheelRoll in MainSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("MainSettings"), LPCTSTR("SleepTime"), LPCTSTR(""), sleep, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute SleepTime in MainSettings not found!");
-
-    //if (GetPrivateProfileString(LPCTSTR("MainSettings"), LPCTSTR("SleepToUpdateTime"), LPCTSTR(""), sleepUpdate, 8, ini.c_str()) == 0)
-        //return e_exit("INI attribute SleepToUpdateTime in MainSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("MainSettings"), LPCTSTR("DrawDelayTime"), LPCTSTR(""), drawDelay, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute DrawDelayTime in MainSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("UpdateButtonCursorPosX"), LPCTSTR(""), UpdateButtonX, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute UpdateButtonCursorPosX not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("UpdateButtonCursorPosY"), LPCTSTR(""), UpdateButtonY, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute UpdateButtonCursorPosY in ClickerSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("FilterMainButtonCursorPosX"), LPCTSTR(""), FilterMainButtonX, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute FilterMainButtonCursorPosX in ClickerSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("FilterMainButtonCursorPosY"), LPCTSTR(""), FilterMainButtonY, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute FilterMainButtonCursorPosY in ClickerSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("FilterButtonCursorPosX"), LPCTSTR(""), FilterButtonX, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute FilterButtonCursorPosX in ClickerSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("FilterButtonCursorPosY"), LPCTSTR(""), FilterButtonY, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute FilterButtonCursorPosY in ClickerSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("FilterListCursorPosX"), LPCTSTR(""), FilterListX, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute FilterListCursorPosX in ClickerSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("FilterListCursorPosY"), LPCTSTR(""), FilterListY, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute FilterListCursorPosY in ClickerSettings not found!");
-
-    if (GetPrivateProfileString(LPCTSTR("ClickerSettings"), LPCTSTR("mouseWheelRollFilter"), LPCTSTR(""), filterWheelRoll, 8, ini.c_str()) == 0)
-        return e_exit("INI attribute mouseWheelRollFilter in ClickerSettings not found!");
-
-    if (not getHandle(browserProcess)) {
-        printf("%s%s%s\n", "Process browser ", browserProcess, " not found!");
+        printf("%s\n", "PowerBI.ini not found!");
         system("pause");
         return 0;
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(MainSettings), LPCTSTR("browserExeProcess"), LPCTSTR(""), browserProcess, 32, ini.c_str()) == 0) {
+        return e_exit("INI attribute browserExeProcess in ", MainSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(MainSettings), LPCTSTR("mouseWheelRoll"), LPCTSTR(""), WheelRoll, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute mouseWheelRoll in ", MainSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(MainSettings), LPCTSTR("SleepTime"), LPCTSTR(""), sleep, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute SleepTime in ", MainSettings, " not found!");
+    }
+    //if (GetPrivateProfileString(LPCTSTR(MainSettings), LPCTSTR("SleepToUpdateTime"), LPCTSTR(""), sleepUpdate, 8, ini.c_str()) == 0) {
+        //return e_exit("INI attribute SleepToUpdateTime in ", MainSettings, " not found!");
+    //}
+    if (GetPrivateProfileString(LPCTSTR(MainSettings), LPCTSTR("DrawDelayTime"), LPCTSTR(""), drawDelay, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute DrawDelayTime in ", MainSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("UpdateButtonCursorPosX"), LPCTSTR(""), UpdateButtonX, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute UpdateButtonCursorPosX in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("UpdateButtonCursorPosY"), LPCTSTR(""), UpdateButtonY, 8, ini.c_str()) == 0) {
+    return e_exit("INI attribute UpdateButtonCursorPosY in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("FilterMainButtonCursorPosX"), LPCTSTR(""), FilterMainButtonX, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute FilterMainButtonCursorPosX in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("FilterMainButtonCursorPosY"), LPCTSTR(""), FilterMainButtonY, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute FilterMainButtonCursorPosY in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("FilterButtonCursorPosX"), LPCTSTR(""), FilterButtonX, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute FilterButtonCursorPosX in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("FilterButtonCursorPosY"), LPCTSTR(""), FilterButtonY, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute FilterButtonCursorPosY in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("FilterListCursorPosX"), LPCTSTR(""), FilterListX, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute FilterListCursorPosX in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("FilterListCursorPosY"), LPCTSTR(""), FilterListY, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute FilterListCursorPosY in ", ClickerSettings, " not found!");
+    }
+
+    if (GetPrivateProfileString(LPCTSTR(ClickerSettings), LPCTSTR("mouseWheelRollFilter"), LPCTSTR(""), filterWheelRoll, 8, ini.c_str()) == 0) {
+        return e_exit("INI attribute mouseWheelRollFilter in ", ClickerSettings, " not found!");
+    }
+
+    if (not getHandle(browserProcess)) {
+        return e_exit("Process browser ", browserProcess, " not found!");
     }
 
     if (WinHwnd != NULL) {
@@ -182,6 +196,8 @@ int main() {
         SecureZeroMemory(&ini, sizeof(ini));
         SecureZeroMemory(current_work_dir, sizeof(current_work_dir));
         SecureZeroMemory(browserProcess, sizeof(browserProcess));
+        SecureZeroMemory(MainSettings, sizeof(MainSettings));
+        SecureZeroMemory(ClickerSettings, sizeof(ClickerSettings));
 
         while (true) {
 
